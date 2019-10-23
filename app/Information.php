e7f35f347012b93977fb52c531f7994d47e72be3
nguyenvtt <quenmatkhaupart3@gmail.com>
2019-10-23 18:55:30 +0700

    first commit

---
+++ app/Information.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Information extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
+}
---
+++ database/migrations/2019_10_17_114417_create_informations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->text('gender');
+            $table->timestamp('birthday');
+            $table->text('address');
+            $table->text('phone_number');
+            $table->text('hobby');
+            $table->text('experience');
+            $table->text('education');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informations');
+    }
+}
